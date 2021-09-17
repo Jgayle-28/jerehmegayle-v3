2e43c11592c517874f813aa3b90c6ca70c1046cc
New Jerehmegayle Hotness <jerehme.gayle@gmail.com>
2021-09-17 10:31:09 -0700

    Added hover state animations to view more buttons on landing page

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -79,28 +79,29 @@ export default function LatestCode({ repositories }) {
           <h1 className='text-6xl lg:text-9xl max-w-lg font-bold text-brandPrimary my-20 md:my-0 md:text-brandPrimary dark:text-gray-600 text-center lg:text-left'>
             Latest Code
           </h1>
-
-          <motion.a
-            initial={{ opacity: 0, x: 80 }}
-            animate={controls}
-            href={`https://github.com/${userData.githubUsername}`}
-            className='mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center text-brandPrimary dark:text-gray-700'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              className='bi bi-arrow-up-right-square'
-              stroke='4'
-              strokeWidth='4'
-              viewBox='0 0 16 16'>
-              <path
-                fillRule='evenodd'
-                d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z'
-              />
-            </svg>
-            <p>View GitHub</p>
-          </motion.a>
+          <motion.div initial={{ opacity: 0, x: 80 }} animate={controls}>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href={`https://github.com/${userData.githubUsername}`}
+              className='mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg dark:shadow-dark-lg text-xl font-semibold flex flex-row space-x-4 items-center text-brandPrimary dark:text-gray-700 transform hover:scale-95 hover:translate-y-1 hover:!shadow-none transition duration-300 ease-out'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='16'
+                height='16'
+                fill='currentColor'
+                className='bi bi-arrow-up-right-square'
+                stroke='4'
+                strokeWidth='4'
+                viewBox='0 0 16 16'>
+                <path
+                  fillRule='evenodd'
+                  d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z'
+                />
+              </svg>
+              <p>View GitHub</p>
+            </a>
+          </motion.div>
         </motion.div>
       </div>
       <motion.div
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
       zIndex: {
         '-1': '-1',
       },
+      boxShadow: {
+        'dark-lg':
+          '0 10px 15px -3px rgba(255, 255, 255, 0.1), 0 4px 6px -2px rgba(255, 255, 255, 0.04)',
+      },
     },
   },
   variants: {
